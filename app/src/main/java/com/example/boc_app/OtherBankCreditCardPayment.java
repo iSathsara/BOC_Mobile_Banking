3f4538bb2ad705285c185b102fb675623a787b33
iSathsara <isuru.dex@gmail.com>
2020-10-06 15:05:39 +0530

    form implemented for other bank credit payments

--- app/src/main/java/com/example/boc_app/OtherBankCreditCardPayment.java
+++ app/src/main/java/com/example/boc_app/OtherBankCreditCardPayment.java
@@ -1,10 +1,8 @@
 package com.example.boc_app;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,35 +11,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private Button otherBankCreditTransBtn;
+public class OtherBankCreditCardPayment extends AppCompatActivity {
+
+    private Button backToTransMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_other_bank_credit_card_payment);
 
         // setting up toolbar
         Toolbar trans_toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(trans_toolbar);
         getSupportActionBar().setTitle("Transactions");
 
-        otherBankCreditTransBtn = (Button) findViewById(R.id.other_bnk_credit_btn);
-        otherBankCreditTransBtn.setOnClickListener(new View.OnClickListener() {
+        backToTransMenu = (Button) findViewById(R.id.obcp_cancel_btn);
+
+        // go back to transaction menu screen
+        backToTransMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchOtherBankCreditPay();
+                finish();
             }
         });
 
     }
 
-    // intent for launch other bank credit card
-    private void launchOtherBankCreditPay(){
-        Intent intent = new Intent(this, OtherBankCreditCardPayment.class);
-        startActivity(intent);
-    }
 
     // set logout icon in app bar
     @Override
@@ -54,28 +50,30 @@ public class MainActivity extends AppCompatActivity {
     // set logout function
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.logout:
 
                 // implement function here
-                Toast.makeText(this,"Logout selected",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Logout selected", Toast.LENGTH_LONG).show();
                 return true;
 
             case R.id.help:
 
                 // implement function here
-                Toast.makeText(this,"help selected",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "help selected", Toast.LENGTH_LONG).show();
                 return true;
 
             case R.id.logout2:
 
                 // implement function here
-                Toast.makeText(this,"Logout selected",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Logout selected", Toast.LENGTH_LONG).show();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
+
+
 }
+
